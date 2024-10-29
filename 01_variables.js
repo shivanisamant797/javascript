63fdd84aa363019abf77d0cfc173d8d94be7e1b0
shivani <shivanisamant787@gmail.com>
2024-10-29 07:09:59 +0530

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,20 @@
+// keywords in javascript
+const accountId=1343434
+let accountEmail="shivanisamant797@gmail.com"
+var accountpassword="12345"
+accountcity="jaiput"//possibilty to declare
+let accountstate;
+
+//accountId=343 // not allowed
+
+accountEmail="shiva89.com"
+accountpassword="34343"
+accountcity="delhi"
+
+/*
+prefer not to use var
+because of issue in block  scope and funtional scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate])
---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using nodejs, not browser
+
+console.log(3+3);
+
+let name="hitest"
+let age=18
+let isLoggedIn=false
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=true/false
+//null=> standalone value ,empty value
+//undefined=>
+//symbol=>unique
+
+
+//object
+ 
+console.log(typeof "hitesh")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
